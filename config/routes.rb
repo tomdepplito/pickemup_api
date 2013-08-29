72f43206efd97e2776ca7ec270bed58ea4e8d5dc
tomdepplito <tomdepplito@gmail.com>
2013-08-28 21:50:29 -0700

    Added job listings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 PickemupApi::Application.routes.draw do
+  #Preferences
   post '/preferences/update_preference' => 'preferences#update_preference'
   get '/preferences/retrieve_preferences' => 'preferences#retrieve_preferences'
   post '/preferences/create_preference' => 'preferences#create_preference'
+  #Job Listings
+  post '/job_listings/update_listing' => 'job_listings#update_listing'
+  get '/job_listings/retrieve_listings' => 'job_listings#retrieve_listings'
+  post '/job_listings/create_listing' => 'job_listings#create_listing'
 end
