55a36f06c7780129ced264d1ceca2ad07410c830
tomdepplito <tomdepplito@gmail.com>
2013-08-28 18:45:45 -0700

    Most recent preference changes DRY'ed up select statement

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,10 +1,34 @@
 class PreferencesController < ApplicationController
   def update_preference
-    preference = Preference.find("user_id: #{params[:preference].slice!(:user_id)}")
-    preference.update_attributes(preference_params)
+    pref_params = params[:preference]
+    preference = Preference.find("user_id: #{pref_params.slice!(:user_id)}")
+    if preference.update_attributes(pref_params)
+      respond_with 200
+    else
+      respond_with 500
+    end
   end
+
+  def retrieve_preferences
+    preferences = Preference.all.query(:remote => query_params[:remote], :us_citizen => query_params[:us_citizen], :fulltime => query_params[:fulltime])
+    preferences.select! do |preference|
+      ((preference.locations & query_params[:locations]).count >= 1) && ((preference.skills & query_params[:skills]).count >= 1)
+    end
+    @user_ids = preferences.map{ |preference| preference.user_id }
+    if @user_ids.count > 0
+      respond_with @user_ids
+    else
+      respond_with 500
+    end
+  end
+
+  private
 
   def preference_params
     params.require(:preference).permit!
+  end
+
+  def query_params
+    params.require(:query).permit!
   end
 end
--- app/models/preference.rb
+++ app/models/preference.rb
@@ -3,5 +3,9 @@ class Preference < Neo4j::Rails::Model
   property :skills, type: String #Serialized Array
   property :us_citizen, type: :boolean
   property :remote, type: :boolean
+  property :fulltime, type: :boolean
   property :user_id, type: :string, index: :exact, unique: true
+
+  validates_presence_of :user_id
+  validates_uniqueness_of :user_id
 end
