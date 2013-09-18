7e05dc0e6eaf0d99b13ad1d2be65766a0bc06311
tomdepplito <tomdepplito@gmail.com>
2013-09-17 18:52:32 -0700

    Added companies controller

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,7 @@
 class Company < Neo4j::Rails::Model
   property :company_id, type: :string, index: :exact, unique: true
   property :name, type: :string
-  property :description, type: :text
+  property :description, type: :string
   property :website, type: :string
   property :industry, type: :string
   property :num_employees, type: :string
@@ -10,7 +10,6 @@ class Company < Neo4j::Rails::Model
   property :tags, type: :string, default: [] #Serialized Array
   property :total_money_raised, type: :string
   property :competitors, type: :string, default: [] #Serialized Array
-  property :sign_in_count, type: :fixnum
   property :size_definition, type: :string
 
   validates_presence_of :company_id
