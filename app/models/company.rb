903cce3fdd0d60fc71003c03eeb0298eaff5e103
tomdepplito <tomdepplito@gmail.com>
2013-09-17 18:47:54 -0700

    Added company model

---
+++ app/models/company.rb
@@ -0,0 +1,18 @@
+class Company < Neo4j::Rails::Model
+  property :company_id, type: :string, index: :exact, unique: true
+  property :name, type: :string
+  property :description, type: :text
+  property :website, type: :string
+  property :industry, type: :string
+  property :num_employees, type: :string
+  property :founded, type: :string
+  property :acquired_by, type: :string
+  property :tags, type: :string, default: [] #Serialized Array
+  property :total_money_raised, type: :string
+  property :competitors, type: :string, default: [] #Serialized Array
+  property :sign_in_count, type: :fixnum
+  property :size_definition, type: :string
+
+  validates_presence_of :company_id
+  validates_uniqueness_of :company_id
+end
