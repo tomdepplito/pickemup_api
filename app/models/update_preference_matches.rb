76ea526605b4e8b67a35f34d1bb7967b959139c6
tomdepplito <tomdepplito@gmail.com>
2013-10-01 20:31:43 -0700

    Commented out queries for now

--- app/models/update_preference_matches.rb
+++ app/models/update_preference_matches.rb
@@ -8,24 +8,21 @@ class UpdatePreferenceMatches
   def start(job_listing_id)
     @queue.publish(job_listing_id)
     if @queue.receive(:timeout => 5_000)
-      update_relevant_preference_matches(job_listing_id)
-      true
-    end
-  end
-
-  def update_revelant_preference_matches(job_listing_id)
-    job_listing = JobListing.find("job_listing_id: #{job_listing_id}")
-    if job_listing
-      query = {:us_citizen => preference.us_citizen, :remote => preference.remote, :fulltime => preference.fulltime}
-      preferences = Preference.all.query(query)
-      if preferences.count > 0
-        all_preferences = preferences.to_a
-        all_preferences.each do |preference|
-          if ((preference.locations & job_listing.locations).count >= 1) && ((preference.skills & preference.skills).count >= 1)
-            set_score(job_listing, preference)
+      job_listing = JobListing.find("job_listing_id: #{job_listing_id}")
+      if job_listing
+        query = {:us_citizen => job_listing.us_citizen, :remote => job_listing.remote, :fulltime => job_listing.fulltime}
+        #preferences = Preference.all.query(query)
+        preferences = Preference.all
+        if preferences.count > 0
+          all_preferences = preferences.to_a
+          all_preferences.each do |preference|
+            if ((preference.locations & job_listing.locations).count >= 1) && ((preference.skills & preference.skills).count >= 1)
+              set_score(job_listing, preference)
+            end
           end
         end
       end
+      true
     end
   end
 end
