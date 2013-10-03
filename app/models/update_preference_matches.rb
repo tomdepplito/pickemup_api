ca1c0856928cc2ec532411ac12bf47a8e87baa71
tomdepplito <tomdepplito@gmail.com>
2013-10-02 20:21:19 -0700

    Reverted back to old code with logging

--- app/models/update_preference_matches.rb
+++ app/models/update_preference_matches.rb
@@ -9,10 +9,12 @@ class UpdatePreferenceMatches
     @queue.publish(job_listing_id)
     if @queue.receive(:timeout => 5_000)
       job_listing = JobListing.find("job_listing_id: #{job_listing_id}")
+      puts "job listing: #{job_listing}"
       if job_listing
         query = {:us_citizen => job_listing.us_citizen, :remote => job_listing.remote, :fulltime => job_listing.fulltime}
         #preferences = Preference.all.query(query)
         preferences = Preference.all
+        puts "preference count: #{preferences.count}"
         if preferences.count > 0
           all_preferences = preferences.to_a
           all_preferences.each do |preference|
