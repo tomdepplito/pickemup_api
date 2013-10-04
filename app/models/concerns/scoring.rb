42c7bdff4d5d55206069f995de81740e4c9a89e4
tomdepplito <tomdepplito@gmail.com>
2013-10-03 21:13:27 -0700

    Add more redis logging

--- app/models/concerns/scoring.rb
+++ app/models/concerns/scoring.rb
@@ -1,9 +1,11 @@
 module Scoring
   extend self
 
-  def set_score(job_listing, preference)
-    algorithm = Algorithm.new(preference, job_listing)
-    key = "score.#{job_listing.job_listing_id}.#{preference.preference_id}"
-    $scores.set(key, {'score' => algorithm.score}.to_json)
+  #def set_score(job_listing, preference)
+  def set_score(job_listing=nil, preference=nil)
+    #algorithm = Algorithm.new(preference, job_listing)
+    #key = "score.#{job_listing.job_listing_id}.#{preference.preference_id}"
+    #$scores.set(key, {'score' => algorithm.score}.to_json)
+    $scores.set("testing", "THIS IS WORKING")
   end
 end
